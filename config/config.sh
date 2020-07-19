b36f607888d709f2d5a9b10bd76d5e60ad933c2b
Evangelos Papakirykou <vpapakir@gmail.com>
2020-07-19 08:43:48 +0000

    refactoring continues

--- config/config.sh
+++ config/config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export RECIPIENT="vpapakir@noa.gr"
-export DATAFILE="/home/ubuntu/networkdata.csv"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export RECIPIENT="enter_a_valid_email_address_here"
+export DATAFILE="${DIR}/topology.csv"
 export COUNT=2
-export PINGLOG="/var/log/PING.log"
+export PINGLOG="/var/log/linux_health_check_ping.log"
